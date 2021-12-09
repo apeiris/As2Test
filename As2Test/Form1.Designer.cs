38be23aa3887ff6fdd2e9b016b9f3d01f7b2cbfb
mapei <mapei@DESKTOP-OURC6P7>
2021-12-09 17:10:46 -0500

    Add project files.

---
+++ As2Test/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace As2Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtEDI = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSendFilePath = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(981, 358);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.txtEDI);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.lblSendFilePath);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(973, 330);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(748, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Send EDI File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtEDI
+            // 
+            this.txtEDI.Location = new System.Drawing.Point(18, 32);
+            this.txtEDI.Multiline = true;
+            this.txtEDI.Name = "txtEDI";
+            this.txtEDI.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtEDI.Size = new System.Drawing.Size(724, 252);
+            this.txtEDI.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Click to Change -->";
+            // 
+            // lblSendFilePath
+            // 
+            this.lblSendFilePath.AutoSize = true;
+            this.lblSendFilePath.Location = new System.Drawing.Point(126, 3);
+            this.lblSendFilePath.Name = "lblSendFilePath";
+            this.lblSendFilePath.Size = new System.Drawing.Size(38, 15);
+            this.lblSendFilePath.TabIndex = 1;
+            this.lblSendFilePath.Text = "label1";
+            this.lblSendFilePath.Click += new System.EventHandler(this.lblSendFilePath_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(973, 330);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 336);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(981, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 358);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Label lblSendFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEDI;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ As2Test/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+
+namespace As2Test
+{
+    public partial class Form1 : Form
+    {
+        public static FileSystemWatcher fsw = null;
+        const string signCert = @"C:\OpenAS2Server\config\as2_certs.p12";
+       // const string receipientCertFile = @"C:\OpenAS2Server\config\partnera.cer";
+        const string receipientCertFile = @"C:\OpenAS2Server\config\as2_certs_p12.cer";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            lblSendFilePath.Text = Properties.Settings.Default.sendFilePath;
+          //  if (!String.IsNullOrEmpty(fsw.Path)) fsw.EnableRaisingEvents = true;
+        }
+        private string _editext;
+        string editext { get { return _editext; } set { _editext = value; } }
+
+        public virtual void setEDIText(String s)
+        {
+
+            if (txtEDI.InvokeRequired)
+                txtEDI.Invoke(new Action(() => txtEDI.Text = s));
+            else
+            {
+                txtEDI.Text = s;
+            }
+        }
+
+        private static async void SendFileToAs2(string path)
+        {
+            string s = "";
+
+
+            byte[] byteArray;
+
+            try
+            {
+                using (FileStream ss = File.Open(path, FileMode.Open))
+                {
+                    ss.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"@openclose{ ex.Message}\n{ex.StackTrace}");
+            }
+        L1:
+            try
+            {
+                using (FileStream SourceStream = File.Open(path, FileMode.Open))
+                {
+                    byteArray = new byte[SourceStream.Length];
+                    await SourceStream.ReadAsync(byteArray, 0, (int)SourceStream.Length);
+                }
+            }
+            catch (FileNotFoundException nf)
+            {
+                Debug.WriteLine($"******nf******\n{nf.Message}\n{nf.StackTrace}");
+                goto L1;
+            }
+            catch (IOException io)
+            {
+                Debug.WriteLine($"******IOIOIO******\n{io.Message}\n{io.StackTrace}");
+                goto L1;
+            }
+            s = System.Text.Encoding.ASCII.GetString(byteArray);
+            Form1 form1 = new Form1();
+            form1.setEDIText(s);
+
+
+
+
+            Debug.WriteLine(s);
+
+            Debug.Indent(); Debug.WriteLine(s); Debug.Unindent();
+
+            ProxySettings proxy = new ProxySettings();
+            Uri uri = new Uri("http://192.168.86.32:10080");
+
+        
+          
+
+
+            AS2Helper.SendFile(uri, $"{path}", byteArray, "MyCompany_OID", "PartnerA_OID", proxy, 1000, signCert, "testas2", receipientCertFile);
+
+
+        }
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(e.FullPath)) return;
+            Debug.WriteLine($"Created: {e.FullPath}");
+
+            SendFileToAs2(e.FullPath);
+
+            File.Delete(e.FullPath);
+        }
+        private static void OnError(object sender, ErrorEventArgs e) => PrintException(e.GetException());
+        private static void PrintException(Exception? ex)
+        {
+            if (ex != null)
+            {
+                Debug.WriteLine($"Message: {ex.Message} ");
+                Debug.WriteLine("Stacktrace:");
+                Debug.WriteLine(ex.StackTrace);
+
+                PrintException(ex.InnerException);
+            }
+        }
+        private void lblSendFilePath_Click(object sender, EventArgs e)
+        {
+
+            using (var dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    Properties.Settings.Default.sendFilePath = dialog.FileName;
+                    lblSendFilePath.Text = dialog.FileName;
+              
+
+             
+                    Properties.Settings.Default.Save();
+
+
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtEDI.Text = "";
+
+            byte[] byteArray;
+         // string fname = @"C:\data\EDI\MyComapny_OID-PartnerA_OID-OrderID-745634.edi";
+            string fname = lblSendFilePath.Text;
+            using (FileStream SourceStream = File.Open(fname, FileMode.Open))
+            {
+                byteArray = new byte[SourceStream.Length];
+                SourceStream.Read(byteArray, 0, (int)SourceStream.Length);
+                //await SourceStream.ReadAsync(byteArray, 0, (int)SourceStream.Length);
+            }
+
+
+            ProxySettings proxy = new ProxySettings();
+            Uri uri = new Uri("http://localhost:10080");
+
+
+            string[] signCertFileName = { @"C:\OpenAS2Server\config\as2_certs.p12", @"C:\OpenAS2Server\config\companyA.jks" };
+
+            string[] receipientCertFile = { @"C:\OpenAS2Server\config\companyb.cer", @"C:\OpenAS2Server\config\companyB.cer" };
+            string [] fromId = { "companyb_OID","MyCompany_OID", "companyA_OID" };
+            string [] toId = { "companya_OID","PartnerA_OID", "companyB_OID" };
+     
+
+            int ix = 0;
+           
+
+            System.Net.HttpStatusCode c = AS2Helper.SendFile(uri, fname, byteArray, fromId[ix], toId[ix], proxy, 10000
+                      , signCertFileName[ix], "testas2", receipientCertFile[ix]);
+            txtEDI.Text = c.ToString();
+  
+        }
+
+       
+    }
+}
